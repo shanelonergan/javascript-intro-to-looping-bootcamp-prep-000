d4fbee3ec1e20a219624c8de39102c67c685bef7
Shane Lonergan <sptlonergan@gmail.com>
2019-07-03 04:17:06 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -32,7 +32,7 @@ function incrementVariable() {
 function doWhileLoop(num) {
   do {
     console.log('I run once regardless');
-  } while (incrementVariable() < 5);
+  } while (incrementVariable() < num);
 }
 
 doWhileLoop(10);
