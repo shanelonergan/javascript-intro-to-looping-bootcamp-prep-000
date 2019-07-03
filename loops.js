de00721a30f65e172c220893ac2fe3b6df8b3a42
Shane Lonergan <sptlonergan@gmail.com>
2019-07-03 03:50:27 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+var strangeLoops = [];
+
+function forLoop(strangeLoops) {
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+      strangeLoops.push = ('I am 1 strange loop.');
+    } else {
+      strangeLoops.push = (`'I am ${i} strange loops.'`);
+    }
+  }
+  return strangeLoops;
+}
+
+forLoop(strangeLoops);
