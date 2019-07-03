0f86133ce13c0d24ba978721bd6cdeb4cf16b9c9
Shane Lonergan <sptlonergan@gmail.com>
2019-07-03 04:27:58 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -22,14 +22,14 @@ function whileLoop(countdown) {
   return 'done';
 }
 
-var i = 0;
-
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
 
 function doWhileLoop(num) {
+  var i = 0;
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+  
   do {
     console.log('I run once regardless');
   } while (incrementVariable() < num);
